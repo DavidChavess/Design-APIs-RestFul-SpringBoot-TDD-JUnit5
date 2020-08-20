7687e7a14a2e287df244d18be1174dc066d94791
David Chaves <davi.ch.fe@gmail.com>
2020-08-19 21:24:50 -0300

    Upload do projeto

---
+++ src/test/java/com/chaves/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaves.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
