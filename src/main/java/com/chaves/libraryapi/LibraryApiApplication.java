71356724404a67e42745fcc8104fa55755377a4f
David Chaves <davi.ch.fe@gmail.com>
2020-10-07 10:25:30 -0300

    Finalizando serviço de email

--- src/main/java/com/chaves/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/chaves/libraryapi/LibraryApiApplication.java
@@ -4,11 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.MethodParameter;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.validation.BindingResultUtils;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @SpringBootApplication
 @EnableScheduling
@@ -17,11 +13,6 @@ public class LibraryApiApplication {
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
-	}
-
-	@Scheduled(cron = "0 35 22 1/1 * ?")
-	public void teste(){
-		System.out.println("Agendamento funcionando com sucesso!");
 	}
 
 	public static void main(String[] args){
